93e8057f4db4642308630cad4b4119ca055d583a
Castrokimaru <castrokimaru@gmail.com>
2026-02-26 17:39:42 +0300

    my roadmap

---
+++ roadmap.go
@@ -0,0 +1,14 @@
+//syntax basics - variables,loops(for loops) and if/else statements.
+//data sctuctures -learn about slices(dynamic arrays),maps(key-value pairs),and structs(custom data types)
+//interfaces -this is go secret sauce for writing flexible,decoupled code
+//concurrency-mastering gos keywords and channels to pass data between task.
+
+//resources
+//A tour of go.
+//go by example.
+//the go playground.
+  
+
+//gofmt. -automatically formats your code to community standards.
+
+-go is statically typed-//means computer needs to know what kind of data is in a variable(interger,string,boolean) before the program runs.
